07c8f8df2e8a997583480b2fe1ffa0d0b4f7e806
Sean Lerner <sean@smallcity.ca>
2016-06-18 13:52:08 -0400

    removed color dep

--- lib/chom.rb
+++ lib/chom.rb
@@ -1,13 +1,12 @@
 require 'chom/version'
 require 'fileutils'
-require 'colorize'
 
 module Chom
   class App
     attr_reader :user
 
     def initialize
-      @user = ENV['SUDO_USER']
+      @user = ENV['SUDO_USER'] || ENV['USER']
     end
 
     def run
@@ -18,23 +17,23 @@ module Chom
     def chown_dir_and_files_recursively
       print "Attempting 'chown -R g+w .' as '#{@user}'... "
       FileUtils.chmod_R 'g+w', '.'
-      puts 'Success!'.green
+      puts 'Success!'
     rescue Errno::EPERM
-      puts 'Failed.'.red
+      puts 'Failed.'
       suggest_running_as_sudo_and_exit
     end
 
     def chmod_dir_and_files_recursively
       print "Attempting 'chmod -R #{@user}:www' as '#{@user}'... "
       FileUtils.chown_R @user, 'www', '.'
-      puts 'Success!'.green
+      puts 'Success!'
     rescue Errno::EPERM
-      puts 'Failed.'.red
+      puts 'Failed.'
       suggest_running_as_sudo_and_exit
     end
 
     def suggest_running_as_sudo_and_exit
-      puts "Try running with 'sudo chom'".yellow
+      puts "Try running with 'sudo chom'."
       exit 1
     end
   end
